58a82615643b09c0ecc6fc45a02e8bc318bf656a
Eugene Barsky <34789976+evb0110@users.noreply.github.com>
2024-10-26 12:43:40 +0300

    first commit

---
+++ src/components/LicenseView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>LicenseView</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/license/[licenseId]/project/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    project
+    <router-view />
+</template>
+
+<style scoped>
+
+</style>
